84ba1dd03b57a0cebbb17041793dff6eb743f9f9
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-04-10 14:33:53 +1000

    Create/read routes for Thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,17 +3,43 @@ const Thought = require("../models/Thought");
 module.exports = {
   //-------------------------------------------------- CREATE //
 
+  // POST: Create New Thought
+  async createThought(req, res) {
+    try {
+      const newThought = await Thought.create(req.body);
+      res.status(200).json(newThought);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+
   //-------------------------------------------------- READ //
-  
+
   // GET: Find All Thoughts
   async findAllThoughts(req, res) {
-    const allThoughts = await Thought.find();
-    res.status(200).json(allThoughts);
+    try {
+      const allThoughts = await Thought.find();
+      res.status(200).json(allThoughts);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+
+  // GET: Find Thought by ID
+  async findThoughtByID(req, res) {
+    try {
+      const thoughtByID = await Thought.findOne({
+        _id: req.params.thoughtID,
+      });
+      res.status(200).json(thoughtByID);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   },
 
   //-------------------------------------------------- UPDATE //
 
   //-------------------------------------------------- DELETE //
 
-  // DELETE: 
+  // DELETE:
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,13 +1,23 @@
 const router = require("express").Router();
 
-const { findAllThoughts } = require("../../controllers/thoughtController");
+const {
+  findAllThoughts,
+  findThoughtByID,
+  createThought,
+} = require("../../controllers/thoughtController");
 
 //-------------------------------------------------- CREATE //
+
+// POST: Create New Thought
+router.route("/").post(createThought);
 
 //-------------------------------------------------- READ //
 
 // GET: Find All Thoughts
 router.route("/").get(findAllThoughts);
+
+// GET: Find Thought by ID
+router.route("/:thoughtID").get(findThoughtByID);
 
 //-------------------------------------------------- UPDATE //
 
