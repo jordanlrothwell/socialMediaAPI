a71231dc4ea6092b85cb4a4d35d1f1ebcc6376f7
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-04-10 13:12:49 +1000

    Added subdocuments/virtual to User model

--- models/User.js
+++ models/User.js
@@ -1,19 +1,43 @@
 const { Schema, model } = require("mongoose");
 const validateEmail = require("../utils/validateEmail");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      validate: [validateEmail, `Please fill a valid email address`],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  email: {
-    type: String,
-    require: true,
-    unique: true,
-    validate: [validateEmail, `Please fill a valid email address`],
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
 const User = model("User", userSchema);
